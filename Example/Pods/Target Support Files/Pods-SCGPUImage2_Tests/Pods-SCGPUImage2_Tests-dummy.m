fc01dbf587bd700c4281dd58cac921fe529dd088
孙超 <sunweichao@rayclear.com>
2023-04-18 18:53:25 +0800

    init 0.1.0

---
+++ Example/Pods/Target Support Files/Pods-SCGPUImage2_Tests/Pods-SCGPUImage2_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SCGPUImage2_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SCGPUImage2_Tests
+@end
---
+++ Example/Pods/Target Support Files/SCGPUImage2/SCGPUImage2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SCGPUImage2 : NSObject
+@end
+@implementation PodsDummy_SCGPUImage2
+@end
